712f2b8620a010dd7b87d79436ce1a7e96478635
FMS-Cat <yutaka.obuchi@gmail.com>
2020-08-26 21:59:31 +0900

    init

---
+++ hello-sdl/hello-sdl.cpp
@@ -0,0 +1,12 @@
+﻿// hello-sdl.cpp : Defines the entry point for the application.
+//
+
+#include "hello-sdl.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
